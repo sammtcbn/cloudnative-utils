8e7662093d6356e4fc2bad2b05056981cf52e6a5
sammtcbn <sammtcbn@gmail.com>
2022-01-09 00:33:39 +0800

    minor update

--- deploy/rabbitmq-install.sh
+++ deploy/rabbitmq-install.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+function change_value()
+{
+  RABBITMQ_VALUEFILE=values.yaml
+
+  echo username is
+  yq e '.auth.username' ${RABBITMQ_VALUEFILE}
+
+  echo password is
+  yq e '.auth.password' ${RABBITMQ_VALUEFILE}
+
+  yq e '.extraPlugins = "rabbitmq_auth_backend_ldap rabbitmq_mqtt"' -i ${RABBITMQ_VALUEFILE}
+  echo extraPlugins change to
+  yq e '.extraPlugins' ${RABBITMQ_VALUEFILE}
+}
+
 pkgname=rabbitmq
 pkgpath=bitnami/${pkgname}
 foldername=rabbitmq
@@ -9,4 +24,5 @@ rm -rf ${foldername} || exit 1
 helm pull ${pkgpath} --untar || exit 1
 
 cd ${foldername} || exit 1
+#change_value
 helm install ${pkgname} -f values.yaml ${pkgpath} || exit 1
