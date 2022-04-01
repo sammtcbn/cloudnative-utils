1b8f71b67e079949b68c9375864531faebeeb5cb
sammtcbn <sammtcbn@gmail.com>
2022-04-01 16:40:20 +0800

    add comment

--- docker-snap-install.sh
+++ docker-snap-install.sh
@@ -2,6 +2,9 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo snap install docker
+
+# Running Docker as normal user
+# refer to https://snapcraft.io/docker
 sudo addgroup --system docker
 sudo adduser $USER docker
 newgrp docker
